e88de533700393cd7e8cf792163487bb22aa48e5
Nick Haliday <nick.haliday@gmail.com>
2012-09-05 15:48:36 -0400

    Finished decode

--- huffman/decode.c
+++ huffman/decode.c
@@ -1,22 +1,28 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define BUFFSIZE 20
-#define MAXENTRY 1000
+#define BUFFSIZE 1000
 
 typedef struct node node;
 struct node {
     char ch;
     node *next[2];
 };
-
-node entry[MAXENTRY];
-
-int nentry = 0;
 node *head;
 
 node *newentry() {
-    return (nentry < MAXENTRY) ? &entry[nentry++] : NULL;
+    return malloc(sizeof(node));
+}
+
+void deltrie(node *p) {
+    if (p->next[0]) {
+        deltrie(p->next[0]);
+    }
+    if (p->next[1]) {
+        deltrie(p->next[1]);
+    }
+    free(p);
 }
 
 node *addedge(node *n, int b) {
@@ -38,23 +44,25 @@ int main() {
     fscanf(fin, "%d", &n);
 
     for (i = 0; i < n; ++i) {
-        fgets(buffer, BUFFSIZE, fin);
+        fscanf(fin, "%s", buffer);
         p = head;
-        for (bit = buffer + 1; *bit != '\n' && *bit != '\0'; ++bit) {
+        for (bit = buffer + 1; *bit; ++bit) {
             p = addedge(p, *bit == '1');
         }
         p->ch = buffer[0];
     }
 
-    fgets(buffer, BUFFSIZE, fin);
+    fscanf(fin, "%s", buffer);
     p = head;
-    for (bit = buffer; *bit != '\n' && *bit != '\0'; ++bit) {
+    for (bit = buffer; *bit; ++bit) {
+        p = p->next[*bit == '1'];
         if (p->ch) {
             putchar(p->ch);
             p = head;
         }
-        p = p->next[*bit == '1'];
     }
+
+    deltrie(head);
 
     putchar('\n');
 }
